0ba43a022dadb13fd124d2f304d546cbea566e7e
Zaid Qureshi <zaidsheikh530@gmail.com>
2025-02-24 18:39:25 +0530

    feature: Add filter in cabin list

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,10 +3,18 @@ import { useCabin } from './cabin.hook'
 import Table from '../../ui/Table'
 import CabinRow from './CabinRow'
 import Menus from '../../ui/Menus'
+import { useSearchParams } from 'react-router'
 
 function CabinTable() {
   const { cabins, isLoading } = useCabin()
+  const [searchParams] = useSearchParams()
+  const discountValue = searchParams.get('discount') || 'all'
   if (isLoading) return <Spinner />
+  let filteredCabins = []
+  if (discountValue === 'all') filteredCabins = cabins
+  else if (discountValue === 'with-discount') filteredCabins = cabins.filter((cabin) => cabin.discount > 0)
+  else if (discountValue === 'no-discount') filteredCabins = cabins.filter((cabin) => cabin.discount === 0)
+
   return (
     <Menus>
       <Table coloumns="0.6fr 1.8fr 2.2fr 1fr 1fr 0.1fr">
@@ -18,7 +26,7 @@ function CabinTable() {
           <div>Discount</div>
           <div></div>
         </Table.Header>
-        <Table.Body data={cabins} render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />} />
+        <Table.Body data={filteredCabins} render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />} />
       </Table>
     </Menus>
   )
--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,11 +1,10 @@
-import SortBy from 'ui/SortBy'
-import Filter from 'ui/Filter'
-import TableOperations from 'ui/TableOperations'
+// import SortBy from '../../ui/SortBy'
+import Filter from '../../ui/Filter'
+import TableOperations from '../../ui/TableOperations'
 
 function CabinTableOperations() {
   return (
     <TableOperations>
-      {/* We could do these two as compound components as well, but let's keep it simple, and let's also explore different ways of achieving the same thing */}
       <Filter
         filterField="discount"
         options={[
@@ -15,7 +14,7 @@ function CabinTableOperations() {
         ]}
       />
 
-      <SortBy
+      {/* <SortBy
         options={[
           { value: 'name-asc', label: 'Sort by name (A-Z)' },
           { value: 'name-desc', label: 'Sort by name (Z-A)' },
@@ -30,7 +29,7 @@ function CabinTableOperations() {
             label: 'Sort by capacity (high first)',
           },
         ]}
-      />
+      /> */}
     </TableOperations>
   )
 }
