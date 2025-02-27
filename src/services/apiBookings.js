ae370230285de3f771d51c280d29ba9d43423c99
Zaid Sheikh <zaidsheikh530@gmail.com>
2025-02-27 22:33:53 +0530

    feature: Start on booking flow

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,6 +1,17 @@
 import { getToday } from '../utils/helpers'
 import supabase from './supabase'
 
+export async function getBookings() {
+  const { data, error } = await supabase.from('bookings').select('*, cabins(*), guests(*)').order('startDate')
+
+  if (error) {
+    console.error(error)
+    throw new Error('Bookings could not get loaded')
+  }
+
+  return data
+}
+
 export async function getBooking(id) {
   const { data, error } = await supabase.from('bookings').select('*, cabins(*), guests(*)').eq('id', id).single()
 
--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 
 const StyledTable = styled.div`
   border: 1px solid var(--color-grey-200);
-
+  margin-top: 2rem;
   font-size: 1.4rem;
   background-color: var(--color-grey-0);
   border-radius: 7px;
